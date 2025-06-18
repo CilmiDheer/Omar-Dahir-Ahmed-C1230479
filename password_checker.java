5db6f29e458bc65ecd41bb65ee27d073e581be98
CilmiDheer <cumardaahiraxmed91@gmail.com>
2025-06-18 11:27:30 +0300

    Add files via upload

---
+++ password_checker.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class password_checker {
+    public static void main (String[]args){
+
+        System.out.println("fadlan soo gali numberka  password ah :  ");
+
+        Scanner input=new Scanner(System.in);
+
+
+        Random random=new Random();
+
+        int tiro= input.nextInt();
+
+
+        String uppurcase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        String lowercase="abcdefghijklmnopqrstuvwxyz";
+
+        String digit="0123456789";
+
+        String specialCharector="$#@!%&*?";
+
+
+        String AllCharectors=uppurcase + lowercase + digit + specialCharector;
+
+        StringBuilder password = new StringBuilder();
+
+        for (int i=0; i<tiro;i++){
+
+            int index = random.nextInt(AllCharectors.length());
+
+            char ch = AllCharectors.charAt(index);
+
+            password.append(ch);
+        }
+        System.out.println("Password-kaagu waa: " + password.toString());
+
+    }
+}
